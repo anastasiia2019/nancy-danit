e8798a8cea383cc783b53fd33c6a0d6c7ed87e5f
Anastasiia Silchenko <ar.silchenko@gmail.com>
2019-04-09 18:27:46 +0300

    add changes to homework 1 js

--- homework-js/javascript/js-homework1/js/hm-1.js
+++ homework-js/javascript/js-homework1/js/hm-1.js
@@ -1,26 +1,25 @@
 "use strict";
 
-function validateNumber(n) {
-    return /^[0-9]+$/.test(n);
+function validateNumber(userAge) {
+    return (userAge < 1 || userAge > 110 ||(isNaN(userAge) ));
 }
 
-function validateName(n) {
-    return /^[a-zA-Z]+$/.test(n);
+function validateName(userName) {
+    return (userName === null  || userName === "");
 }
 
 let userName = prompt('Enter your name. ');
+while (validateName(userName)) {
+    userName = prompt("Please, entered your name if you forgot or check the correct entered your name", userName);
+}
 let userAge = prompt('Enter your age. ');
+while (validateNumber(userAge)) {
+    userAge = prompt("Please, check the correct entered information", userAge);
+}
+
 let urAllowed = 'Welcome, ';
 let urNotAllowed = 'You are not allowed to visit this website.';
 let areUsure = 'Are you sure you want to continue?';
-
-while (!validateName(userName)) {
-    userName = prompt("Please, entered your name if you forgot or check the correct entered your name", userName);
-}
-while (!validateNumber(userAge) && userAge != null || userAge > 110) {
-    userAge = alert("Please, check the correct entered information. It must be only number, not null and less then 110 years");
-    userAge = prompt('How old are you?')
-}
 if (userAge > 22) {
     alert(urAllowed + userName)
 } else if (userAge >= 18 && userAge <= 22) {
@@ -33,5 +32,4 @@ if (userAge > 22) {
 } else {
     alert(urNotAllowed)
 }
-
 
