bb541e849d5baf5a595129d137169763142479cf
Anastasiia Silchenko <ar.silchenko@gmail.com>
2019-05-13 16:44:47 +0300

    add homework 6js-array

---
+++ homework-js/javascript/js-homework6/js/filterBy.js
@@ -0,0 +1,20 @@
+'use strict';
+let arr = [48,25, 'str1','str2',true,false,null, undefined,[2,3,4], {key1:5,key2:6}];
+
+function filterBy(array, type) {
+    let newArr = [];
+    
+    arr.forEach(function(item) {
+            if (typeof (item) != type) {
+                newArr.push(item);
+            }
+    });
+    return newArr;
+}
+
+// console.log( filterBy(arr, 'boolean'));
+console.log( filterBy(arr, 'number'));
+// console.log( filterBy(arr, 'string'));
+// console.log( filterBy(arr, 'undefined'));
+// console.log(filterBy(arr, "null"));
+// console.log( filterBy(arr, 'object'));
