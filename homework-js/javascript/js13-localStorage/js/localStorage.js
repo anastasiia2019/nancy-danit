11f6aec12ce4b7bd4070cf74e537cbea02c92af0
Anastasiia Silchenko <ar.silchenko@gmail.com>
2019-05-27 18:53:21 +0300

    add js13 localStorage to check

---
+++ homework-js/javascript/js13-localStorage/js/localStorage.js
@@ -0,0 +1,45 @@
+
+let currentTheme = "default";
+
+function changeTheme() {
+    window.localStorage.setItem("theme", currentTheme);
+
+    let oldlink = document.getElementsByTagName("link").item(0);
+
+    let newlink = document.createElement("link");
+    newlink.setAttribute("rel", "stylesheet");
+    newlink.setAttribute("type", "text/css");
+    newlink.setAttribute("href", "css/" + currentTheme + ".css");
+
+    document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    currentTheme = window.localStorage.getItem("theme");
+
+    if (currentTheme != null) {
+        changeTheme();
+    }
+    else {
+        currentTheme = "default";
+    }
+
+});
+
+
+function changeCSS() {
+    if (currentTheme == "alt") {
+        currentTheme = "default";
+    }
+    else {
+        currentTheme = "alt";
+    }
+
+    changeTheme();
+
+}
+
+
+change.addEventListener("click", changeCSS);
